a03b7ff84e78e297859dd4e5068353a92908ecc3
Bastien Beurier <bastienbeurier@gmail.com>
2013-08-15 16:06:53 -0700

    Move to production

--- street-shout-ios/Utilities/AFStreetShoutAPIClient.m
+++ street-shout-ios/Utilities/AFStreetShoutAPIClient.m
@@ -10,7 +10,7 @@
 #import "AFJSONRequestOperation.h"
 #import "GeneralUtilities.h"
 
-static NSString * const kAFStreetShoutAPIBaseURLString = @"http://dev-street-shout.herokuapp.com/";
+static NSString * const kAFStreetShoutAPIBaseURLString = @"http://street-shout.herokuapp.com/";
 
 @implementation AFStreetShoutAPIClient
 
