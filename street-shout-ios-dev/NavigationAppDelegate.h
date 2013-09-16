dfeb4534474ed419459c7ab2a1039cd7dd47a17d
Bastien Beurier <bastienbeurier@gmail.com>
2013-09-16 11:19:59 +0200

    Add build target for Shatte

---
+++ street-shout-ios-dev/NavigationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NavigationAppDelegate.h
+//  street-shout-ios-dev
+//
+//  Created by Bastien Beurier on 9/16/13.
+//  Copyright (c) 2013 Street Shout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
